2f03229edbfc9458246a7b956ea618edd30c0755
kananpatel27 <kananlpatel27@gmail.com>
2026-03-15 17:43:45 +0530

    Add files via upload

---
+++ Find duplicate numbers.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 2, 5, 3, 6]
+
+seen = set()
+duplicates = set()
+
+for num in numbers:
+    if num in seen:
+        duplicates.add(num)
+    else:
+        seen.add(num)
+
+print("Duplicate numbers:", list(duplicates))
